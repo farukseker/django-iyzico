ce9d7e861f932b751a75293ca1b8434283132910
F4ruk.seker <ahmetfaruk731@gmail.com>
2025-03-11 08:12:33 +0300

    Add Front-End

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="w-full min-h-screen flex flex-col bg-gray-900 text-white">
+    <nav class="mx-auto mt-2">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  <router-view class="mx-auto" />
+  </main>
+</template>
+
+<style>
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,56 @@
+<template>
+  <h2 class="text-xl font-bold">Ödeme Formu</h2>
+
+  <div class="home flex m-auto gap-2 relative">
+    <fieldset class="fieldset w-xs bg-gray-750 border border-gray-400 font-semibold text-white p-4 rounded-box">
+      <legend class="fieldset-legend text-white">Ürünleriniz</legend>
+      
+    </fieldset>
+
+    <fieldset class="fieldset w-xs bg-gray-750 border border-gray-400 font-semibold text-white p-4 rounded-box">
+      <legend class="fieldset-legend text-white">Alıcı Adresi</legend>
+      
+      <div class="flex gap-2">
+        <div>
+          <label class="fieldset-label text-white">Alıcı Adı</label>
+          <input type="text" class="input" placeholder="Name" />
+        </div>
+        <div>
+          <label class="fieldset-label text-white">Alıcı Soyadı</label>
+          <input type="text" class="input" placeholder="Name" />
+        </div>
+      </div>
+      
+      <label class="fieldset-label text-white">Adres adı <span class="text-gray-500">(Optional)</span></label>
+      <input type="text" class="input" placeholder="My awesome page" />
+      
+      <label class="fieldset-label text-white">Ülke</label>
+      <input type="text" class="input" placeholder="my-awesome-page" />
+       
+      <label class="fieldset-label text-white">şehir</label>
+      <input type="text" class="input" placeholder="my-awesome-page" />      
+      
+      <label class="fieldset-label text-white">ilçe</label>
+      <input type="text" class="input" placeholder="my-awesome-page" />
+
+      <label class="fieldset-label text-white">posta kodu</label>
+      <input type="text" class="input" placeholder="my-awesome-page" />
+      
+      <legend class="fieldset-legend text-white">Adresiniz</legend>
+      <textarea class="textarea h-24" placeholder="Bio"></textarea>
+    </fieldset>
+
+    <fieldset class="fieldset w-xs bg-gray-750 border border-gray-400 font-semibold text-white p-4 rounded-box">
+      <legend class="fieldset-legend text-white">Ödeme bilgileri</legend>
+      
+    </fieldset>
+
+    <div class="absolute right-0 -bottom-14 ">
+      <button class="btn btn-primary">Ödeme Yap</button>
+    </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
